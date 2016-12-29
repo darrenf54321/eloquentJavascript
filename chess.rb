7f54cca9b1dac2aedb6e01d2ce76be80c67f0590
Darrenf <darrenf54321@gmail.com>
2016-12-29 12:08:05 +0000

    Methods added

--- chess.rb
+++ chess.rb
@@ -1,10 +1,18 @@
-size = 8
-hash1 = "# "
-hash2 = " #"
-count = size
+class Chess
 
-while (count > 0)
-  puts (hash1 * size)
-  puts (hash2 * size)
-count -= 2
+  def output(num)
+    size = num
+    hash1 = "# "
+    hash2 = " #"
+    count = size
+
+      while (count > 0)
+        puts (hash1 * size)
+        puts (hash2 * size)
+      count -= 2
+      end
+  end
 end
+
+game = Chess.new
+game.output(4)
